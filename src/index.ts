ee34613cafd43015225e5d32e44448a6961033cf
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2024-03-10 23:46:35 +0100

    Update .gitignore and index.ts, modify serverless.yml and tsconfig.json, and update package.json

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
+import _ from 'lodash';
+
 export const handler = async () => {
     return {
         statusCode: 200,
         body: JSON.stringify({
-            message: 'Hello World from Lambda!',
+            message: `Hello World from Lambda! ${_.now()}`,
         }),
     };
 };
